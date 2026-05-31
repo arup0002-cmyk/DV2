dcbf01564ea24cbbf0136f1bd130f46ae78378c7
arup0002-cmyk <arup0002@student.monash.edu>
2026-05-31 20:25:37 +1000

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -8,6 +8,8 @@ var vg_7 = "charts/chart7_age_trends_lines.vg.json";
 var vg_8 = "charts/chart8_lifestyle_comparison.vg.json";
 var vg_9 = "charts/chart9_year_slider_bars.vg.json";
 var vg_10 = "charts/chart10_interactive_drilldown.vg.json";
+var vg_11 = "charts/chart11_faceted_age_activities.vg.json";
+var vg_12 = "charts/chart12_heatmap_state_activities.vg.json";
 
 vegaEmbed("#chart1", vg_1);
 vegaEmbed("#chart2", vg_2);
@@ -19,3 +21,5 @@ vegaEmbed("#chart7", vg_7);
 vegaEmbed("#chart8", vg_8);
 vegaEmbed("#chart9", vg_9);
 vegaEmbed("#chart10", vg_10);
+vegaEmbed("#chart11", vg_11);
+vegaEmbed("#chart12", vg_12);
