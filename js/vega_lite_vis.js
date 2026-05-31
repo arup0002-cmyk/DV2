d39c6ad889acbf50bb5e3bd232798650c6ddd7a3
arup0002-cmyk <arup0002@student.monash.edu>
2026-05-31 17:46:45 +1000

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,3 @@
-// js/vega_lite_vis.js
-
 var vg_1 = "charts/chart1_internet_trend.vg.json";
 var vg_2 = "charts/chart2_map_state_usage.vg.json";
 var vg_3 = "charts/chart3_slope_lifestyle.vg.json";
@@ -11,13 +9,13 @@ var vg_8 = "charts/chart8_lifestyle_comparison.vg.json";
 var vg_9 = "charts/chart9_year_slider_bars.vg.json";
 var vg_10 = "charts/chart10_interactive_drilldown.vg.json";
 
-vegaEmbed("#chart1", vg_1).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart2", vg_2).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart3", vg_3).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart4", vg_4).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart5", vg_5).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart6", vg_6).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart7", vg_7).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart8", vg_8).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart9", vg_9).then(function(result) {}).catch(console.error);
-vegaEmbed("#chart10", vg_10).then(function(result) {}).catch(console.error);
+vegaEmbed("#chart1", vg_1);
+vegaEmbed("#chart2", vg_2);
+vegaEmbed("#chart3", vg_3);
+vegaEmbed("#chart4", vg_4);
+vegaEmbed("#chart5", vg_5);
+vegaEmbed("#chart6", vg_6);
+vegaEmbed("#chart7", vg_7);
+vegaEmbed("#chart8", vg_8);
+vegaEmbed("#chart9", vg_9);
+vegaEmbed("#chart10", vg_10);
